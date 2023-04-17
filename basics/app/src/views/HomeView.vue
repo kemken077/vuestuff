a54a2471ec901343d4a270e4c00373267ef3b49d
¨kemken077¨ <kemken077@gmail.com>
2023-04-17 10:18:19 -0300

    Template syntax: event binding, attr, event handling, dynamic HTML insertion, methods

---
+++ basics/app/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <h1>The basics</h1>
+    <p>Welcome to VueJS basics</p>
+  </main>
+</template>
---
+++ basics/app/src/views/TemplateSyntax.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export default {
+    data() {
+      return {
+        dynamicHTML: `<code>Dynamic HTML</code>`,
+        isButtonDisabled: false,
+      }
+    },
+    methods: {
+      handleCheckboxState(event: any) {
+        console.log({isChecked: event.target.checked});
+        this.isButtonDisabled = event.target.checked;
+      },
+      handleCTAClick(event: any) {
+        console.log('CTA clicked!', event);
+      }
+    },
+  }
+</script>
+<template>
+  <div class="template-syntax">
+    <h1 :id="'title'">{{ 'Template Syntax:' }}</h1>
+    <p class="wrapper" v-html="dynamicHTML"></p> <!-- will PARSE as HTML --> 
+    <p>{{ dynamicHTML }}</p> <!-- will NOT parse as HTML -->
+    <button :disabled="isButtonDisabled" @click="handleCTAClick">Call to action</button>
+    <input type="checkbox" name="cta" :id="'ctaChackbox'" v-on="{ change: handleCheckboxState }" />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
